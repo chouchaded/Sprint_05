4aed417b4f11562600eacba5d59957765443ec62
YanSub <chouchade@gmail.com>
2022-03-03 08:38:22 +0400

    Modif Mercredi 03/03

--- index.js
+++ index.js
@@ -1,20 +1,43 @@
+async function fetchRestCountrie() {
+  const response = await fetch("https://restcountries.com/v3.1/region/europe");
+  const countries = await response.json();
+  return countries;
+}
+fetchRestCountrie().then(countries => {
+    countries; 
+    console.log(countries);
+    for (let i = 0; i < countries.length; i++) {
 
- fetch("https://restcountries.com/v3.1/region/europe"
-)
-    .then(function (res) {
-    
-        return res.json();
-    })
-     .then(function (data) {
-        console.log(data);
-         for (let i = 0; i < data.length; i++) {
-
-             let nameRest = data[i].name.official;
+             let nameRest = countries[i].name.official;
     console.log(nameRest);
             let html=document.getElementById('list');
             let name=document.createElement('li');
          name.textContent = nameRest;
             html.appendChild(name);
                  }
+});
+
+
+
+
+//  fetch("https://restcountries.com/v3.1/region/europe"
+// )
+//     .then(function (res) {
+    
+//         return res.json();
+//     })
+//      .then(function (data) {
+//         console.log(data);
+//          for (let i = 0; i < data.length; i++) {
+
+//              let nameRest = data[i].name.official;
+//     console.log(nameRest);
+//             let html=document.getElementById('list');
+//             let name=document.createElement('li');
+//          name.textContent = nameRest;
+//             html.appendChild(name);
+//                  }
          
-    });
+//     });
+
+   
